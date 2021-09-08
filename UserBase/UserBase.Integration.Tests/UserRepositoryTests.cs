412eea8216772dc041501d41ee8459aa42d85758
willjames <me@willjames.co.uk>
2021-09-08 14:18:37 -0400

    added NUnit test runner package reference. added local connection string to test class - WIP

--- UserBase/UserBase.Integration.Tests/UserRepositoryTests.cs
+++ UserBase/UserBase.Integration.Tests/UserRepositoryTests.cs
@@ -1,14 +1,26 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 
 namespace UserBase.Integration.Tests
 {
     [TestFixture]
     public class UserRepositoryTests
     {
+        //odd issue - unable to read connection string from web.config
+        //postponing resolution as would be solving wrong problem for purposes of this exercise
 
-        [Test]
+        //readonly string connectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+        readonly string connectionString = @"data source=DESKTOP-RUDASU2\SQLEXPRESS;Initial Catalog=UserBase;Integrated Security=SSPI;";
+
+        //[Test]
+        //public void Can_get_connection_string_from_web_config()
+        //{
+        //    Assert.That(connectionString, Is.Not.Empty);
+        //}
+
+        [Test, Ignore("WIP")]
         public void Can_insert_new_user_record()
         {
             int userRecordCount;
